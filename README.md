42ab72cf859802ba8ce17e46d3f3657821fac0af
Dmitry Dubina <dmitryd.prog@gmail.com>
2025-03-10 00:39:36 +0700

    Update README.md

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 A simple plugin for syntax highlighting of the Odin programming language.  
 It adds highlighting for keywords, built-in types, and function names.
 
-![image](https://github.com/user-attachments/assets/706f4126-4b3a-42ea-bd94-4431a1e9396a)
+Example:
+
+![image](https://github.com/user-attachments/assets/8148b1b8-f901-4995-a3c8-3c7a7b7e7bd2)
+
 
--- odin-simple-mode.el
+++ odin-simple-mode.el
@@ -13,7 +13,7 @@
   '("nil" "true" "false"))
 
 (defun odin/keywords ()
-  '("import" "foreign" "package" "typeid" "when" "where" "if" "else" "for" "switch" "in" "not_in" "do" "case" "break" "continue" "fallthrough" "defer" "return" "proc" "struct" "union" "enum" "bit_set" "bit_field" "map" "dynamic" "auto_cast" "cast" "transmute" "distinct" "using" "context" "or_else" "or_return" "or_break" "or_continue" "asm" "matrix" "size_of" "align_of" "len"))
+  '("import" "foreign" "package" "typeid" "when" "where" "if" "else" "for" "switch" "in" "not_in" "do" "case" "break" "continue" "fallthrough" "defer" "return" "proc" "struct" "union" "enum" "bit_set" "bit_field" "map" "dynamic" "auto_cast" "cast" "transmute" "distinct" "using" "context" "or_else" "or_return" "or_break" "or_continue" "asm" "matrix"))
 
 (defun odin/types ()
   '("byte" "bool" "b8" "b16" "b32" "b64" "i8" "u8" "i16" "u16" "i32" "u32" "i64" "u64" "i128" "u128" "rune" "f16" "f32" "f64" "complex32" "complex64" "complex128" "quaternion64" "quaternion128" "quaternion256" "int" "uint" "uintptr" "rawptr" "string" "cstring" "typeid" "any" "i16le" "u16le" "i32le" "u32le" "i64le" "u64le" "i128le" "u128le" "i16be" "u16be" "i32be" "u32be" "i64be" "u64be" "i128be" "u128be" "f16le" "f32le" "f64le" "f16be" "f32be" "f64be"))
