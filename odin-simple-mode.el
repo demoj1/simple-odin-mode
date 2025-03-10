db81c4c6624e40e6503689f8e83bfc0804244fc4
Dmitry Dubina <dmitryd.prog@gmail.com>
2025-03-10 11:26:09 +0700

    Add error parse in compilation mode
    Add customization

--- odin-simple-mode.el
+++ odin-simple-mode.el
@@ -1,6 +1,37 @@
 ;;; odin-simple-mode.el --- sample major mode for editing Odin. -*- coding: utf-8; lexical-binding: t; -*-
 
-(require 'font-lock)
+(require 'compile)
+
+(defgroup odin-simple-mode nil
+  "Odin simple mode."
+  :group 'tools)
+
+(defcustom odin/compile-command nil
+  "Command use in compile mode (if not set, use Odin path)"
+  :type '(choice
+          string
+          (const :tag "Use odin path" nil))
+  :group 'odin-simple-mode)
+
+(defcustom odin/path "odin"
+  "Path to executable odin"
+  :type 'file
+  :group 'odin-simple-mode)
+
+(defcustom odin/constants '("nil" "true" "false")
+  "Syntax specific word"
+  :type '(repeat string)
+  :group 'odin-simple-mode)
+
+(defcustom odin/keywords '("import" "foreign" "package" "typeid" "when" "where" "if" "else" "for" "switch" "in" "not_in" "do" "case" "break" "continue" "fallthrough" "defer" "return" "proc" "struct" "union" "enum" "bit_set" "bit_field" "map" "dynamic" "auto_cast" "cast" "transmute" "distinct" "using" "context" "or_else" "or_return" "or_break" "or_continue" "asm" "matrix")
+  "Syntax specific word"
+  :type '(repeat string)
+  :group 'odin-simple-mode)
+
+(defcustom odin/types '("byte" "bool" "b8" "b16" "b32" "b64" "i8" "u8" "i16" "u16" "i32" "u32" "i64" "u64" "i128" "u128" "rune" "f16" "f32" "f64" "complex32" "complex64" "complex128" "quaternion64" "quaternion128" "quaternion256" "int" "uint" "uintptr" "rawptr" "string" "cstring" "typeid" "any" "i16le" "u16le" "i32le" "u32le" "i64le" "u64le" "i128le" "u128le" "i16be" "u16be" "i32be" "u32be" "i64be" "u64be" "i128be" "u128be" "f16le" "f32le" "f64le" "f16be" "f32be" "f64be")
+  "Syntax specific word"
+  :type '(repeat string)
+  :group 'odin-simple-mode)
 
 (defvar odin-simple-mode-syntax-table
   (let ((table (make-syntax-table)))
@@ -9,19 +40,10 @@
     (modify-syntax-entry ?\n "> b" table)
     table))
 
-(defun odin/constants ()
-  '("nil" "true" "false"))
-
-(defun odin/keywords ()
-  '("import" "foreign" "package" "typeid" "when" "where" "if" "else" "for" "switch" "in" "not_in" "do" "case" "break" "continue" "fallthrough" "defer" "return" "proc" "struct" "union" "enum" "bit_set" "bit_field" "map" "dynamic" "auto_cast" "cast" "transmute" "distinct" "using" "context" "or_else" "or_return" "or_break" "or_continue" "asm" "matrix"))
-
-(defun odin/types ()
-  '("byte" "bool" "b8" "b16" "b32" "b64" "i8" "u8" "i16" "u16" "i32" "u32" "i64" "u64" "i128" "u128" "rune" "f16" "f32" "f64" "complex32" "complex64" "complex128" "quaternion64" "quaternion128" "quaternion256" "int" "uint" "uintptr" "rawptr" "string" "cstring" "typeid" "any" "i16le" "u16le" "i32le" "u32le" "i64le" "u64le" "i128le" "u128le" "i16be" "u16be" "i32be" "u32be" "i64be" "u64be" "i128be" "u128be" "f16le" "f32le" "f64le" "f16be" "f32be" "f64be"))
-
 (defvar odin/fontlock
-  (list (cons (regexp-opt (odin/keywords) 'symbols) 'font-lock-keyword-face)
-        (cons (regexp-opt (odin/types) 'symbols) 'font-lock-type-face)
-        (cons (regexp-opt (odin/constants) 'symbols) 'font-lock-constant-face)
+  (list (cons (regexp-opt odin/keywords 'symbols) 'font-lock-keyword-face)
+        (cons (regexp-opt odin/types 'symbols) 'font-lock-type-face)
+        (cons (regexp-opt odin/constants 'symbols) 'font-lock-constant-face)
         (cons (rx (group "#" (? "+") letter (minimal-match (0+ graph))) (or "(" "{" "[" whitespace)) '(1 font-lock-builtin-face))
         (cons (rx bol (0+ whitespace) "package" whitespace (group letter (0+ graph))) '(1 font-lock-builtin-face))
         (cons (rx bol (0+ whitespace) "import" whitespace (group letter (0+ graph))) '(1 font-lock-builtin-face))
@@ -29,11 +51,19 @@
         (cons (rx bol (0+ whitespace) (group letter (0+ graph)) (1+ whitespace) "::" whitespace (1+ any)) '(1 font-lock-constant-face)))
   "list font locks")
 
+;;;###autoload
 (define-derived-mode odin-simple-mode prog-mode "Odin mode"
   :syntax-table odin-simple-mode-syntax-table
   (setq-local
    comment-start "// "
-   font-lock-defaults '((odin/fontlock)))
+   font-lock-defaults '((odin/fontlock))
+   compile-command (format "%s " (or odin/compile-command odin/path)))
+  (cl-pushnew
+   'odin
+   compilation-error-regexp-alist)
+  (cl-pushnew
+   '(odin "^\\([^[:space:]]+?\\)(\\([[:digit:]]+\\):\\([[:digit:]]+\\)) .*$" 1 2 3 nil)
+   compilation-error-regexp-alist-alist)
   (indent-tabs-mode t))
 
 ;;;###autoload
